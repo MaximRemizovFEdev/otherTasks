1a0607a46b1cd950e9e98677186e74521b31d2fd
remizov.ms <remizov.m@sarex.io>
2025-12-11 14:36:18 +0300

    feat: init task 1

---
+++ task1.js
@@ -0,0 +1,27 @@
+// [ALGS]: Числовой палиндром https://www.youtube.com/watch?v=rD1l1i7_fwc
+
+// const input1 = 121; // true
+// const input2 = -121; // false
+// const input3 = 0; // false
+
+// const isPalindromNumber = (x) => {};
+
+// console.log(isPalindromNumber(input1));
+// console.log(isPalindromNumber(input2));
+// console.log(isPalindromNumber(input3));
+
+const input1 = 121; // true
+const input2 = -121; // false
+const input3 = 0; // false
+
+const isPalindromNumber = (x) => {
+    if (typeof x !== 'number') {
+        throw "is not a number"
+    }
+
+
+};
+
+console.log(isPalindromNumber(input1));
+console.log(isPalindromNumber(input2));
+console.log(isPalindromNumber(input3));
