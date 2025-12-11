4aada2b56bc7d19bf8814d5374e890ef7787ad1f
remizov.ms <remizov.m@sarex.io>
2025-12-11 15:06:33 +0300

    feat: resolve task

--- task1.js
+++ task1.js
@@ -15,13 +15,26 @@ const input2 = -121; // false
 const input3 = 0; // false
 
 const isPalindromNumber = (x) => {
-    if (typeof x !== 'number') {
-        throw "is not a number"
+  if (typeof x !== "number") {
+    throw "is not a number";
+  }
+
+  if (x <= 0) {
+    return false;
+  }
+
+  const numArr = String(x).split("");
+
+  for (let i = 0; i < Math.floor(numArr.length / 2); i++) {
+    if (numArr[i] !== numArr[numArr.length - 1 - i]) {
+      return false;
     }
+  }
 
-
+  return true;
 };
 
+console.log(isPalindromNumber(123));
 console.log(isPalindromNumber(input1));
 console.log(isPalindromNumber(input2));
 console.log(isPalindromNumber(input3));
